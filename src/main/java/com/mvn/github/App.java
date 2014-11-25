d2f2d77ffd55f089f239ce8119c837f8d6892943
Shutty <shutty.github@gmail.com>
2014-11-26 01:22:39 +0700

    Update App.java

--- src/main/java/com/mvn/github/App.java
+++ src/main/java/com/mvn/github/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "HW6-Github Test success");
         System.out.println( "Congratulation!!!" );
+        System.out.println( "Test edit from github" );
+        //Test edit from github and pull into local then push new revision back to server
     }
 }

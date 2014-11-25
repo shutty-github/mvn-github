5289ea31730735662ab52f0a95e5ffd497f21c4a
shutty <shutty.github@gmail.com>
2014-11-26 01:18:31 +0700

    4th Commit

--- src/main/java/com/mvn/github/App.java
+++ src/main/java/com/mvn/github/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "HW6-Github Test success");
+        System.out.println( "Congratulation!!!" );
     }
 }
